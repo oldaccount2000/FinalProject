4a348ffd42d6d6ad2c385185921eec4e3e6dd96f
Rima.Shahbazyan <rima.shahbazyan@synisys.com>
2019-11-02 16:01:12 +0400

    commiting sample component and haf of sectors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent} from './container/container.component';
+import { SampleComponent } from './container/sample/sample.component';
+import { SectorsComponent } from './container/sectors/sectors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    SampleComponent,
+    SectorsComponent
   ],
   imports: [
     BrowserModule,
